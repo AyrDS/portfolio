bf78eea4f8d1a696d9bdb70fe9a1d9a60597b237
AyrDS <ayrtonds97@gmail.com>
2022-05-10 15:13:11 -0300

    Menu responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import OffCanvas from 'react-aria-offcanvas';
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOpen = () => {
+        setIsOpen(true);
+    }
+
+    const handleClose = () => {
+        setIsOpen(false);
+    }
+
     return (
         <header className='header-container' id='home' >
             <p className='header-logo' >  Ayr<span>DS</span> </p>
@@ -23,6 +36,51 @@ const Navbar = () => {
                     </li>
                 </ul>
             </nav>
+
+            <button
+                className="header-burger"
+                id='menu-button'
+                onClick={handleOpen}
+            >
+                <span className="burger" ></span>
+                <span className="burger" ></span>
+                <span className="burger" ></span>
+            </button>
+
+            <OffCanvas
+                isOpen={isOpen}
+                onClose={() => setIsOpen(false)}
+                labelledby="menu-button"
+                position='right'
+                height='100vh'
+                width='65vw'
+                className='slider-menu'
+            >
+                <nav className='nav-mobile' >
+                    <ul>
+                        <li onClick={handleClose} >
+                            <a href='#sobreMi'>
+                                Sobre Mi
+                            </a>
+                        </li>
+                        <li onClick={handleClose} >
+                            <a href='#proyectos'>
+                                Proyectos
+                            </a>
+                        </li>
+                        <li onClick={handleClose} >
+                            <a href='#contacto'>
+                                Contacto
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <button className='button-offcanvas' onClick={handleClose} >
+                    <span className="burger-offcanvas span-1" ></span>
+                    <span className="burger-offcanvas span-2" ></span>
+                </button>
+            </OffCanvas>
         </header>
     )
 }
